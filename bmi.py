f60c6d830637ad72a57e367f51c80fe2c0e953de
phoo <PHOOPYAESA28.24@ichat.sp.edu.sg>
2025-05-23 18:43:47 +0800

    modified bmi with return value

--- bmi.py
+++ bmi.py
@@ -4,6 +4,16 @@ def calculate_bmi(height, weight):
 
     bmi = weight /(height*height)
 
+    if bmi < 18.5:  
+        print("Under Weight")
+        return -1
+    elif (18.5 <= bmi <= 25.0):
+        print("Normal Weight")
+        return 0
+    elif bmi>25.0 : 
+        print("Over Weight")
+        return 1
+
     print(bmi)
 
 
